80fe442a07718a67640894c9b3d9f55df248dae6
Michelle <mchristine.stone91@gmail.com>
2022-03-23 20:42:40 -0400

    Added basic Header outline information.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App">Hi.</div>;
+  return (
+    <div className="App">
+      <div className="flex-column justify-flex-start">
+        <Header />
+      </div>
+      <p className="main-info">Hi.</p>
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-const Home = () => {};
+const Header = () => {
+  return (
+    <header className="nav-bar">
+      <h1>Full-Stack E-Commerce</h1>
+      <nav className="text-center">
+        <p>Login</p>
+        <p>Logout</p>
+        <p>Signup</p>
+      </nav>
+    </header>
+  );
+};
 
-export default Home;
+export default Header;
