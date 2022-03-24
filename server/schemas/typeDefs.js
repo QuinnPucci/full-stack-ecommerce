eb652454fef75e4873cda59da4c419125edc26a9
DYoshizawa079 <daniely079@gmail.com>
2022-03-23 22:54:41 -0400

    Optimized it for user model only

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,28 +13,13 @@ const typeDefs = gql`
         token: ID!
         user: User
     }
-    type Product {
-        name: String
-        description: String
-        image: String
-        price: Float
-        quantity: Int
-        category: Category
-    }
     type Query {
         findUsers: [User]
         findUser(username: String!): User
-        categories: [Category]
-        products:(category: ID, name: String): [Product]
-        product(_id: ID!): Product
-        user: User
-        order(_id: ID!): Order
     }
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        addOrder(products: [ID]!) Order
-        updateproduct(_id: ID!, quantity: Int!): Product 
     }
     `;
 
