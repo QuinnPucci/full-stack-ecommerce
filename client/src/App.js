366f55e0eaf3b248dbcbe4cf02d9bde9f808d656
QuinnPooch <pucciquinn@gmail.com>
2022-03-29 14:49:04 -0600

    working

--- client/src/App.js
+++ client/src/App.js
@@ -14,19 +14,15 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
-// ------- For JWT stuff -------
-
-// const authLink = setContext((_, { headers }) => {
-//   const token = localStorage.getItem('id_token');
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : '',
-//     },
-//   };
-// });
-
-// -------------
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
