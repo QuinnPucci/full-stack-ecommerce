7a2016a36c5f94494faf589fc230fd18e4a6a636
QuinnPooch <pucciquinn@gmail.com>
2022-03-29 13:58:38 -0600

    added apollo to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,41 @@
 import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// ------- For JWT stuff -------
+
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem('id_token');
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
+
+// -------------
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className="App">
       <div className="flex-column justify-flex-start">
         <Header />
@@ -13,6 +45,7 @@ function App() {
         <Footer />
       </div>
     </div>
+    </ApolloProvider>
   );
 }
 
