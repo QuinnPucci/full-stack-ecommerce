8c17b49db89f5fc7bcc92abbb3d0c8ac857093c5
Michelle <mchristine.stone91@gmail.com>
2022-03-29 17:31:57 -0400

    Added button styling

--- client/src/App.js
+++ client/src/App.js
@@ -4,35 +4,25 @@ import {
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
-<<<<<<< HEAD
-// ------- For JWT stuff -------
-
-=======
->>>>>>> 366f55e0eaf3b248dbcbe4cf02d9bde9f808d656
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
-<<<<<<< HEAD
-
-=======
->>>>>>> 366f55e0eaf3b248dbcbe4cf02d9bde9f808d656
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
@@ -42,15 +32,15 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-    <div className="App">
-      <div className="flex-column justify-flex-start">
-        <Header />
-        <div className="container">
-          <p className="main-info">Hi.</p>
+      <div className="App">
+        <div className="flex-column justify-flex-start">
+          <Header />
+          <div className="container">
+            <p className="main-info">Hi.</p>
+          </div>
+          <Footer />
         </div>
-        <Footer />
       </div>
-    </div>
     </ApolloProvider>
   );
 }
