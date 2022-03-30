d92b173e8ca2899e9aba9b4cd12a245f9a910293
AdamCampeau <adamcampeau3@gmail.com>
2022-03-30 14:25:41 -0400

    addtocart

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,13 @@ import CartPage from "./pages/CartPage";
 import Product from './pages/Product';
 import Profile from './pages/Profile';
 
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Badge from "@material-ui/core/Badge";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
+
 const httpLink = createHttpLink({
   uri: "/graphql",
 });
@@ -62,4 +69,35 @@ function App() {
   );
 }
 
-export default App;
+export default App() {
+  const [count, setCount] = React.useState(1);
+  
+  return (
+    <div style={{ display: "block", padding: 30 }}>
+      <div>
+        <Badge color="secondary" badgeContent={count}>
+          <ShoppingCartIcon />{" "}
+        </Badge>
+        <ButtonGroup>
+          <Button class='add-to-cart-btn'
+            onClick={() => {
+              setCount(Math.max(count - 1, 0));
+            }}
+          >
+            {" "}
+            <RemoveIcon fontSize="medium" />
+          </Button>
+          <Button
+            onClick={() => {
+              setount(count + 1);
+            }}
+          >
+            {" "}
+            <AddIcon fontSize="medium" />
+          </Button>
+        </ButtonGroup>
+      </div>
+    </div>
+  );
+}
+}
--- client/src/components/AddToCart/index.js
+++ client/src/components/AddToCart/index.js
@@ -1,14 +1,39 @@
-import { React } from 'react';
+import React from "react";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Badge from "@material-ui/core/Badge";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
 
-const addToCart = () => { document.querySelectorAll(".add-to-cart");  
- document.querySelectorAll('.add-to-cart-btn').forEach(function(addToCart) {  
-   addToCartBtn.addEventListener('click', function() {  
-     addToCartBtn.classList.add('added');  
-     setTimeout(function(){  
-       addToCartBtn.classList.remove('added');  
-     }, 2001);  
-   });  
- })
+export default function App() {
+  const [count, setCount] = React.useState(1);
+  
+  return (
+    <div style={{ display: "block", padding: 30 }}>
+      <div>
+        <Badge color="secondary" badgeContent={count}>
+          <ShoppingCartIcon />{" "}
+        </Badge>
+        <ButtonGroup>
+          <Button class='remove-from-cart-btn'
+            onClick={() => {
+              setCount(Math.max(count - 1, 0));
+            }}
+          >
+            {" "}
+            <RemoveIcon fontSize="medium" />
+          </Button>
+          <Button class='add-to-cart-btn'
+            onClick={() => {
+              setount(count + 1);
+            }}
+          >
+            {" "}
+            <AddIcon fontSize="medium" />
+          </Button>
+        </ButtonGroup>
+      </div>
+    </div>
+  );
 }
- 
- export default addToCart;
